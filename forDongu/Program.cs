0efcd07c083c62974e35ee1bb0d4f6f246e6ba98
Trabzon Ticaret <trabzonticaret2021@hotmail.com>
2022-04-05 10:46:06 +0300

    döngü içi boş kutu yada sıfır görünümü oluşturmak

---
+++ forDongu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forDongu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            for(int i = 1; i<=30; i++)
+            {
+               for(int j = 1; j<=10; j++)
+                {
+                    if(i==1 || i==30 || j==1 || j==10)
+                    {
+
+                        if (j == 10 )
+                        {
+                            Console.WriteLine(" * ");
+
+                        }
+                        else
+                        {
+                            Console.Write(" * ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+                       
+
+                  
+                }  
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
